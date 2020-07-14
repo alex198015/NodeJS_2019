975192a7b0f5c9a921bc59b9274a0286db5c7aec
alex198015 <kitten20811@gmail.com>
2020-07-14 10:32:56 +0300

    express part 5

--- index_express.js
+++ index_express.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
+const hbs = exphbs.create({
+    defaultLayout : 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views2')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     // res.status(200)
-    res.sendFile(path.join(__dirname, 'views2', 'index.html'))
+    // res.sendFile(path.join(__dirname, 'views2', 'index.html'))
+    res.render('index')
 })
 app.get('/about', (req, res) => {
     // res.status(200)
-    res.sendFile(path.join(__dirname, 'views2', 'about.html'))
+    // res.sendFile(path.join(__dirname, 'views2', 'about.html'))
+    res.render('about')
 })
 
 const PORT = process.env.PORT || 3000
