9608f91d5115df6ff45fa03e5fd62d8545c039a6
alex198015 <kitten20811@gmail.com>
2020-07-19 22:25:08 +0300

    mongoDb part 4

--- index_express.js
+++ index_express.js
@@ -5,12 +5,17 @@ const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const cardRoutes = require('./routes/card')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const hbs = exphbs.create({
     defaultLayout : 'main',
-    extname: 'hbs'
+    extname: 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+      }
 })
 
 app.engine('hbs', hbs.engine)
@@ -24,6 +29,27 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+
+const PORT = process.env.PORT || 3000
+
+
+async function start() {
+    try {
+        const url = `mongodb+srv://Alex80:UHR1cFeLDNZejuMz@cluster0.ru3vg.mongodb.net/shop`
+        await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+        app.listen(PORT, () => {
+            console.log(` Server is running on port ${PORT}`);
+        
+    })
+
+    } catch(e) {
+        console.log(e);
+    }
+    
+}
+
+start()
+
 
 // app.get('/', (req, res) => {
 //     // res.status(200)
@@ -49,10 +75,3 @@ app.use('/card', cardRoutes)
 //         isCourses: true
 //     })
 // })
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-    console.log(` Server is running on port ${PORT}`);
-    
-})
