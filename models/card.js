eec39dbff38e62ff38cf9a2edaab6b213a2b4bb0
alex198015 <kitten20811@gmail.com>
2020-07-22 23:13:12 +0300

    mongoDB part 8

--- models/card.js
+++ models/card.js
@@ -1,76 +1,76 @@
-const path = require('path')
-const fs = require('fs')
+// const path = require('path')
+// const fs = require('fs')
 
-const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data', 'card.json'
-)
+// const p = path.join(
+//     path.dirname(process.mainModule.filename),
+//     'data', 'card.json'
+// )
 
-class Card {
-    static async add(course) {
-        const card = await Card.fetch()
+// class Card {
+//     static async add(course) {
+//         const card = await Card.fetch()
 
-        const idx = card.courses.findIndex(c => c.id === course.id)
-        const candidate = card.courses[idx]
-        if(candidate) {
-            // курс уже есть
-            candidate.count++
-            card.courses[idx] = candidate
-        }else {
-            // нужно добавить курс
-            course.count = 1
-            card.courses.push(course)
-        }
-        card.price += +course.price
+//         const idx = card.courses.findIndex(c => c.id === course.id)
+//         const candidate = card.courses[idx]
+//         if(candidate) {
+//             // курс уже есть
+//             candidate.count++
+//             card.courses[idx] = candidate
+//         }else {
+//             // нужно добавить курс
+//             course.count = 1
+//             card.courses.push(course)
+//         }
+//         card.price += +course.price
 
-        return new Promise((resolve, reject) => {
-            fs.writeFile(p, JSON.stringify(card), (err) => {
-                if(err) {
-                    reject(err)
-                } else {
-                    resolve()
-                }
-            })
-        })
+//         return new Promise((resolve, reject) => {
+//             fs.writeFile(p, JSON.stringify(card), (err) => {
+//                 if(err) {
+//                     reject(err)
+//                 } else {
+//                     resolve()
+//                 }
+//             })
+//         })
 
-    }
+//     }
 
-    static async remove(id) {
-        const card = await Card.fetch()
-        const idx = card.courses.findIndex(c => c.id === id)
-        const course = card.courses[idx]
+//     static async remove(id) {
+//         const card = await Card.fetch()
+//         const idx = card.courses.findIndex(c => c.id === id)
+//         const course = card.courses[idx]
 
-        if(course.count === 1) {
-            // удалить
-            card.courses.splice(idx, 1)
-        } else {
-            // изменить количество
-            course.count--
-        }
-        card.price -= course.price
+//         if(course.count === 1) {
+//             // удалить
+//             card.courses.splice(idx, 1)
+//         } else {
+//             // изменить количество
+//             course.count--
+//         }
+//         card.price -= course.price
 
-        return new Promise((resolve, reject) => {
-            fs.writeFile(p, JSON.stringify(card), (err) => {
-                if(err) {
-                    reject(err)
-                } else {
-                    resolve(card)
-                }
-            })
-        })
-    }
+//         return new Promise((resolve, reject) => {
+//             fs.writeFile(p, JSON.stringify(card), (err) => {
+//                 if(err) {
+//                     reject(err)
+//                 } else {
+//                     resolve(card)
+//                 }
+//             })
+//         })
+//     }
 
-    static async fetch() {
-        return new Promise((resolve, reject) => {
-            fs.readFile(p, 'utf-8', (err, content) => {
-                if (err) {
-                    reject(err)
-                } else {
-                    resolve(JSON.parse(content))
-                }
-            })
-        })
-    }
-}
+//     static async fetch() {
+//         return new Promise((resolve, reject) => {
+//             fs.readFile(p, 'utf-8', (err, content) => {
+//                 if (err) {
+//                     reject(err)
+//                 } else {
+//                     resolve(JSON.parse(content))
+//                 }
+//             })
+//         })
+//     }
+// }
 
-module.exports = Card
+// module.exports = Card
--- routes/card.js
+++ routes/card.js
@@ -1,12 +1,20 @@
 const {Router} = require('express')
-const Card = require('../models/card')
+// const Card = require('../models/card')
 const Course = require('../models/course')
 
 const router = Router()
 
+// router.post('/add', async (req, res) => {
+//     const course = await Course.getById(req.body.id)
+//     await Card.add(course)
+//     res.redirect('/card')
+   
+// })
+
 router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id)
-    await Card.add(course)
+    // const course = await Course.getById(req.body.id)
+    const course = await Course.findById(req.body.id)
+    await req.user.addToCart(course)
     res.redirect('/card')
    
 })
@@ -17,13 +25,14 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    const card = await Card.fetch()
-    res.render('card', {
-        title: 'Корзина',
-        isCard: true,
-        courses: card.courses,
-        price: card.price
-    })
+    // const card = await Card.fetch()
+    // res.render('card', {
+    //     title: 'Корзина',
+    //     isCard: true,
+    //     courses: card.courses,
+    //     price: card.price
+    // })
+    res.json({test: true})
 })
 
 
