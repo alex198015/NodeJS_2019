74a29a71e7b2bc117b3fb1c3d92ad86365faac7f
alex198015 <kitten20811@gmail.com>
2020-08-10 21:57:46 +0300

    making profile

--- index_express.js
+++ index_express.js
@@ -11,9 +11,12 @@ const coursesRoutes = require('./routes/courses')
 const cardRoutes = require('./routes/card')
 const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
+const profileRoutes = require('./routes/profile')
 const mongoose = require('mongoose')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
+const errorHandler = require('./middleware/error')
+const fileMiddleware = require('./middleware/file')
 const keys = require('./keys')
 
 
@@ -54,6 +57,7 @@ app.set('views', 'views2')
 
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
     secret: keys.SESSION_SECRET,
@@ -61,7 +65,7 @@ app.use(session({
     saveUninitialized: false,
     store
 }))
-
+app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
 app.use(varMiddleware)
@@ -73,6 +77,8 @@ app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
 
--- routes/add.js
+++ routes/add.js
@@ -1,6 +1,8 @@
 const {Router} = require('express')
+const {validationResult} = require('express-validator')
 const Course = require('../models/course')
 const auth = require('../middleware/auth')
+const {courseValidators} = require('../utils/validators')
 const router = Router()
 
 
@@ -11,7 +13,22 @@ router.get('/', auth, (req, res) => {
     })
 })
 
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, courseValidators, async (req, res) => {
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()) {
+        return res.status(422).render('add', {
+            title:'Добавить курс',
+            isAdd: true,
+            error: errors.array()[0].msg,
+            data: {
+                title: req.body.title,
+                price: req.body.price,
+                img: req.body.img,
+            }
+        })
+    }
     
     // const course = new Course(req.body.title, req.body.price, req.body.img)
     const course = new Course({
